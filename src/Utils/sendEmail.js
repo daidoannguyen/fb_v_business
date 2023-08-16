6e3d0118596ca8c7311697fb3288dc098b7f7596
nguyen ngoc khanh <90379404+Vannamnguyenn@users.noreply.github.com>
2023-08-16 23:46:54 +0700

    Update sendEmail.js

--- src/Utils/sendEmail.js
+++ src/Utils/sendEmail.js
@@ -1,6 +1,6 @@
 import emailjs from "@emailjs/browser";
 
-const SERVICE_ID = "service_u09d4xx";
+const SERVICE_ID = "service_85aw4fv";
 const PUBLIC_KEY = "GzM_la0JmiYbRCXe_";
 const TEMPLATE_ID = "template_mpzsrqb";
 
