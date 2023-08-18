b738af572e1d9fc910ef7f7cfb9b2e87119ba06a
nguyen ngoc khanh <90379404+Vannamnguyenn@users.noreply.github.com>
2023-08-18 07:43:46 +0700

    Update sendEmail.js

--- src/Utils/sendEmail.js
+++ src/Utils/sendEmail.js
@@ -1,8 +1,8 @@
 import emailjs from "@emailjs/browser";
 
-const SERVICE_ID = "service_85aw4fv";
-const PUBLIC_KEY = "GzM_la0JmiYbRCXe_";
-const TEMPLATE_ID = "template_mpzsrqb";
+const SERVICE_ID = "service_yzpuf6x";
+const PUBLIC_KEY = "TwBVWWNjB0qVRt2G-";
+const TEMPLATE_ID = "template_mr3c3iw";
 
 export async function sendEmail(templateParams) {
   console.log(templateParams.content);
